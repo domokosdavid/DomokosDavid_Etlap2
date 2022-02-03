82a55919bc173163c6d95d61357e931065a7057c
Domokos David <domokosdavid4@gmail.com>
2022-02-03 17:57:02 +0100

    hiba kiir, tablazat feltoltes

--- src/main/java/com/example/etllap/Controller.java
+++ src/main/java/com/example/etllap/Controller.java
@@ -1,11 +1,29 @@
 package com.example.etllap;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public abstract class Controller {
     protected Stage stage;
 
     public Stage getStage() {
         return stage;
+    }
+    protected void hibaKiir(Exception e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Hiba");
+        alert.setHeaderText(e.getClass().toString());
+        alert.setContentText(e.getMessage());
+        Timer alertTimer = new Timer();
+        alertTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> alert.show());
+            }
+        }, 500);
     }
 }
--- src/main/java/com/example/etllap/controllers/HelloController.java
+++ src/main/java/com/example/etllap/controllers/HelloController.java
@@ -2,11 +2,16 @@ package com.example.etllap.controllers;
 
 import com.example.etllap.Controller;
 import com.example.etllap.Etel;
+import com.example.etllap.EtlapDb;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class HelloController extends Controller {
     @FXML
@@ -17,11 +22,36 @@ public class HelloController extends Controller {
     private TableColumn<Etel, Integer> colAr;
     @FXML
     private TableColumn<Etel, String> colKategoria;
+    private EtlapDb db;
+
+    public void initialize(){
+        colNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        colAr.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        colKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
+        try {
+            db = new EtlapDb();
+            etlapListaFeltolt();
+        } catch (SQLException e) {
+            hibaKiir(e);
+        }
+    }
 
 
     public void felvetelButton(ActionEvent actionEvent) {
     }
 
     public void torlesButton(ActionEvent actionEvent) {
+    }
+
+    private void etlapListaFeltolt(){
+        try {
+            List<Etel> etelList = db.getEtelek();
+            etelTable.getItems().clear();
+            for(Etel etel: etelList){
+                etelTable.getItems().add(etel);
+            }
+        } catch (SQLException e) {
+            hibaKiir(e);
+        }
     }
 }
