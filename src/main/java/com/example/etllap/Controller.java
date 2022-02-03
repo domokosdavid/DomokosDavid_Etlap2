574fe2efb48ab9128879aa2e08d89cca2287ee9a
Domokos David <domokosdavid4@gmail.com>
2022-02-03 17:48:35 +0100

    controller absctract osztaly

---
+++ src/main/java/com/example/etllap/Controller.java
@@ -0,0 +1,11 @@
+package com.example.etllap;
+
+import javafx.stage.Stage;
+
+public abstract class Controller {
+    protected Stage stage;
+
+    public Stage getStage() {
+        return stage;
+    }
+}
