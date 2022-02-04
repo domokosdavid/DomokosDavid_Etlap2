9625182bdf3746667c320b253f5b1ea654a0c2bf
Domokos David <domokosdavid4@gmail.com>
2022-02-04 12:59:49 +0100

    hozzaad müködik, hibak javitva

--- src/main/java/com/example/etllap/controllers/HozzaadController.java
+++ src/main/java/com/example/etllap/controllers/HozzaadController.java
@@ -2,12 +2,16 @@ package com.example.etllap.controllers;
 
 import com.example.etllap.Controller;
 import com.example.etllap.EtlapDb;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class HozzaadController extends Controller {
 
@@ -16,25 +20,30 @@ public class HozzaadController extends Controller {
     @FXML
     private TextArea inputLeiras;
     @FXML
-    private ChoiceBox inputKategoria;
+    private ChoiceBox<String> inputKategoria;
     @FXML
     private Spinner<Integer> inputAr;
 
     private EtlapDb db;
 
+    public void initialize() throws SQLException{
+        inputKategoria.getSelectionModel().selectFirst();
+        db = new EtlapDb();
+    }
+
     @FXML
-    public void hozzaadButtonClick(ActionEvent actionEvent) {
+    public void hozzaadButtonClick(ActionEvent actionEvent){
         String nev = inputNev.getText().toString();
         String leiras = inputLeiras.getText().toString();
-        Object kategoria = inputKategoria.getValue().toString();
+        String kategoria = inputKategoria.getSelectionModel().getSelectedItem();
+
         int ar = inputAr.getValue();
         try {
-            db = new EtlapDb();
-            int siker = db.etelHozzaadasa(nev, leiras, ar, (String) kategoria);
+            int siker = db.etelHozzaadasa(nev, leiras, ar, kategoria);
             if (siker == 1){
-                alert("Film hozzáadása sikeres");
+                alert("Étel hozzáadása sikeres");
             } else {
-                alert("Film hozzáadása sikeretelen");
+                alert("Étel hozzáadása sikeretelen");
             }
         } catch (Exception e) {
             hibaKiir(e);
