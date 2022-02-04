23da0d2a95cb456c70faa002fe818ba4cc657b01
Domokos David <domokosdavid4@gmail.com>
2022-02-04 13:39:30 +0100

    torles javitva, ha nincs kivalasztva torolni kivant elem, lefut az alert

--- src/main/java/com/example/etllap/controllers/HelloController.java
+++ src/main/java/com/example/etllap/controllers/HelloController.java
@@ -53,10 +53,12 @@ public class HelloController extends Controller {
 
     public void torlesButton(ActionEvent actionEvent) throws SQLException{
         Etel etel = etelTable.getSelectionModel().getSelectedItem();
-        if (beleegyezes("Biztos, hogy ki szeretnéd törölni a kiválasztott ételt?")){
-            db.etelTorles(etel.getId());
-            etlapListaFeltolt();
-            return;
+        if (etel != null){
+            if (beleegyezes("Biztos, hogy ki szeretnéd törölni a kiválasztott ételt?")){
+                db.etelTorles(etel.getId());
+                etlapListaFeltolt();
+                return;
+            }
         }
         alert("Válaszd ki a törölni kívánt ételt!");
     }
